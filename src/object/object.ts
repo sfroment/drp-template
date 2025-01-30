4cd27f11c615d527f597f6fa0cb2d7c07cd8095d
Sacha Froment <sfroment42@gmail.com>
2025-01-30 08:20:27 +0100

    123
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- src/object/object.ts
+++ src/object/object.ts
@@ -4,16 +4,86 @@ import {
 	type ResolveConflictsType,
 	SemanticsType,
 	type Vertex,
-} from "@ts-drp/object";
+} from "@sfroment/object";
+
+import { getColorForPeerId } from "../utils/colors";
+
+type Insert = {
+	peerId: string;
+	content: string;
+	color: string;
+};
 
 export class ExampleDRPObject implements DRP {
 	semanticsType = SemanticsType.pair;
 
-	//constructor() {}
+	private _inserts: Insert[] = [];
 
-	resolveConflicts(_: Vertex[]): ResolveConflictsType {
-		return {
-			action: ActionType.Nop,
-		};
+	constructor() {
+		this._inserts = [];
+	}
+
+	insert(peerId: string, index: number, content: string) {
+		if (content.length === 0) return;
+
+		this._inserts.splice(index, 0, {
+			peerId,
+			content,
+			color: getColorForPeerId(peerId),
+		});
+	}
+
+	delete(index: number, length: number) {
+		this._inserts.splice(index, length);
+	}
+
+	// insert -> first 
+	// insert -> delete 
+	// insert -> insert 
+	// insert the insert hisght peerID 1st
+	// delete the delete highest peerID 1st
+	resolveConflicts(vertices: Vertex[]): ResolveConflictsType {
+		if (vertices.length <= 1 || vertices[0].hash === vertices[1].hash) { return { action: ActionType.Nop }; }
+		
+		const opInsert = "insert";
+		const opDelete = "delete";
+
+		const left = vertices[0];
+		const right = vertices[1];
+		const leftOp = left.operation?.opType;
+		const rightOp = right.operation?.opType;
+
+		if (leftOp  === rightOp) {
+			const leftPeerId = left.peerId;
+			const rightPeerId = right.peerId;
+			const leftContent = left.operation?.value[2];
+			const rightContent = right.operation?.value[2];
+			if (leftPeerId > rightPeerId) {
+				console.log("resolveConflicts1 leftPeerId > rightPeerId\n", leftPeerId, rightPeerId, leftContent, rightContent, vertices[0].hash, vertices[1].hash);
+				return { action: ActionType.Swap };
+			}
+			if (leftPeerId === rightPeerId && leftContent < rightContent) {
+				console.log("resolveConflicts2\n");
+				return {action: ActionType.Swap};
+			}
+			console.log("resolveConflicts3\n");
+			return {action: ActionType.Nop};
+		}
+
+		if (leftOp === opDelete && rightOp === opInsert) {
+			console.log("resolveConflicts4\n");
+			return {action: ActionType.Swap};
+		}
+
+		console.log("resolveConflicts5\n");
+		return {action: ActionType.Nop};
+	}
+
+	query_insert(): Insert[] {
+		return this._inserts;
+	}
+
+	query_text(): string {
+		return this._inserts.map((insert) => insert.content).join("");
 	}
 }
