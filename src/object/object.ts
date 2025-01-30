53b927f5f2a1a0099609f682b4bc664abac6ffc3
Sacha Froment <sfroment42@gmail.com>
2025-01-30 12:57:32 +0100

    toto
    Signed-off-by: Sacha Froment <sfroment42@gmail.com>

--- src/object/object.ts
+++ src/object/object.ts
@@ -5,13 +5,73 @@ import {
 	SemanticsType,
 	type Vertex,
 } from "@ts-drp/object";
+import { getColorForPeerId } from "../utils/colors";
+
+type Insert = {
+	content: string;
+	color: string;
+}
 
 export class ExampleDRPObject implements DRP {
 	semanticsType = SemanticsType.pair;
 
-	//constructor() {}
+	private _insert: Insert[];
 
-	resolveConflicts(_: Vertex[]): ResolveConflictsType {
+	constructor() {
+		this._insert = [];
+	}
+
+	insert(peerId: string, index: number, content:string) {
+		if (content.length === 0) return;
+
+		this._insert.splice(index, 0, {
+			content,
+			color: getColorForPeerId(peerId),
+		});
+	}
+
+	delete(index: number, length: number) {
+		this._insert.splice(index, length);
+	}
+
+	query_insert(): Insert[] {
+		return this._insert;
+	}
+
+	query_text(): string {
+		return this._insert.map((insert) => insert.content).join("");
+	}
+
+	resolveConflicts(vertices: Vertex[]): ResolveConflictsType {
+		if (vertices.length <= 1 && vertices[0].hash === vertices[1].hash) {
+			return {
+				action: ActionType.Nop,
+			};
+		}
+
+		const left = vertices[0];
+		const right = vertices[1];
+		const leftOp = left.operation?.opType;
+		const rightOp = right.operation?.opType;
+
+		if (leftOp === rightOp) {
+			if (left.peerId< right.peerId) {
+				return {
+					action: ActionType.Swap,
+				};
+			}
+
+			return {
+				action: ActionType.Nop,
+			};
+		}
+
+		if (leftOp === "delete" && rightOp === "insert") {
+			return {
+				action: ActionType.Swap,
+			};
+		}
+
 		return {
 			action: ActionType.Nop,
 		};
